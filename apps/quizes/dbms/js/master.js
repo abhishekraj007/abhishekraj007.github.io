9231f1f0e697aa44f46f755eca50440a6b8d23a8
Abhishek Raj <abhishekrajinfo@gmail.com>
2016-12-09 07:53:03 +0000

    addes more question

--- apps/quizes/dbms/js/master.js
+++ apps/quizes/dbms/js/master.js
@@ -1,24 +1,42 @@
 // Put all your page JS here
 
 $(function () {
-    $('#slickQuiz').slickQuiz();
-    
+    $('#slickQuiz').slickQuiz({
+        
     // apply custom option this quiz
-    checkAnswerText("Check My Answer!");
-    nextQuestionText("Next Question");
-    completeQuizText("Get Your Result!");
-    tryAgainText("Try Again!");
-    preventUnansweredText("Please select atleast one!");
-    numberOfQuestions(5);
-    randomSortQuestions(true);
-    randomSortAnswers(true);
-    preventUnanswered(true);
-    perQuestionResponseMessaging(true);
-    // Default: true; - Displays correct / incorrect response messages after each question is submitted.
-    completionResponseMessaging(false);
-    //Default: false; - Displays all questions and answers with correct or incorrect response messages when the quiz is completed.
-    disableRanking (false); 
-    //Default: false; - Removes the ranking leve from the final results display. Eliminates the need for an element with class quizLevel in the markup, as well as the need for JSON values for level1 through level5.
+   
+   
+                checkAnswerText:  'Check My Answer!',
+                nextQuestionText: 'Next Question',
+                backButtonText: '',
+                completeQuizText: 'Get My Result!',
+                tryAgainText: 'Try Again!',
+                questionCountText: 'Question %current of %total',
+                preventUnansweredText: 'You must select at least one answer.',
+                questionTemplateText:  '%count. %text',
+                scoreTemplateText: '%score / %total',
+                nameTemplateText:  '<span>Quiz: </span> DBMS',
+                skipStartButton: false,
+                numberOfQuestions: 5,
+                //Default: null; - the number of questions to load from the question set in the JSON object, defaults to null (all questions); Note: If you set this to an integer, you'll probably also want to set randomSortQuestions to true to ensure that you get a mixed set of questions each page load.
+                randomSortQuestions: true,
+                //Default: false; - whether or not to randomly sort questions ONLY
+                randomSortAnswers: true,
+                //Default: false; - whether or not to randomly sort answers ONLY
+                preventUnanswered: true,
+                //Default: false; - prevents submitting a question with zero answers
+                disableScore: false,
+                disableRanking: false,
+                scoreAsPercentage: false,
+                perQuestionResponseMessaging: true,
+                //Default: true; - Displays correct / incorrect response messages after each question is submitted.
+                perQuestionResponseAnswers: false,
+                // Default: false; - Keeps the answer options in display after the question is submitted. Note: this should be used in tandem with perQuestionResponseMessaging
+                completionResponseMessaging: false,
+                //Default: false; - Displays all questions and answers with correct or incorrect response messages when the quiz is completed.
+                
+    });
+    
     
     
 });
