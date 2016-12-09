31407adceb25a224023da662a128fcf2afb574e7
Abhishek Raj <abhishekrajinfo@gmail.com>
2016-12-09 07:38:24 +0000

    addes more question

--- apps/quizes/dbms/js/master.js
+++ apps/quizes/dbms/js/master.js
@@ -2,4 +2,23 @@
 
 $(function () {
     $('#slickQuiz').slickQuiz();
+    
+    // apply custom option this quiz
+    checkAnswerText("Check My Answer!");
+    nextQuestionText("Next Question");
+    completeQuizText("Get Your Result!");
+    tryAgainText("Try Again!");
+    preventUnansweredText("Please select atleast one!");
+    numberOfQuestions(5);
+    randomSortQuestions(true);
+    randomSortAnswers(true);
+    preventUnanswered(true);
+    perQuestionResponseMessaging(true);
+    // Default: true; - Displays correct / incorrect response messages after each question is submitted.
+    completionResponseMessaging(false);
+    //Default: false; - Displays all questions and answers with correct or incorrect response messages when the quiz is completed.
+    disableRanking (false); 
+    //Default: false; - Removes the ranking leve from the final results display. Eliminates the need for an element with class quizLevel in the markup, as well as the need for JSON values for level1 through level5.
+    
+    
 });
--- apps/quizes/dbms/js/slickQuiz-config.js
+++ apps/quizes/dbms/js/slickQuiz-config.js
@@ -5,13 +5,13 @@
 var quizJSON = {
     "info": {
         "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "main":    "<p>Let's see how well you are prepared!</p>",
+        "results": "<h5>I think you should try one more test</h5><p></p>",
+        "level1":  "Wao! You are real champion!",
+        "level2":  "Wao! Very soon you'll become a champion.",
+        "level3":  "Good. Next time prepared well.",
+        "level4":  "Whatt? Not prepared??",
+        "level5":  "God! You need to work extremely hard kid..." // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
