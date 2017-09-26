9cbd2b358932d68cf79b997a54f7c14112b7b557
Abhishek Raj <abhishek@lollypop.biz>
2017-09-26 22:14:24 +0530

    new data

---
+++ apps/engmania/dict-a.js
@@ -0,0 +1,17 @@
+[
+	{
+		"id": 0,
+		"word":"A surge of anger",
+		"type": "[Noun + Noun]",
+		"meaning": "a powerful rush of an emotion or feeling of anger.",
+		"example": "Susan felt a surge of anger.<br> Lucy felt a surge of anger as she watched her ex-husband enter the room with his new girlfriend."
+	},
+	{
+		"id": 1,
+		"word":"A sense of pride",
+		"type": "[Noun + Noun]",
+		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
+		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
+	}
+
+]
