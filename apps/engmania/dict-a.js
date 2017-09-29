e832aa910df0f3c80aa4dace8c6dc292d7eeef4e
Abhishek Raj <abhishek@lollypop.biz>
2017-09-30 00:52:31 +0530

    update

--- apps/engmania/dict-a.js
+++ apps/engmania/dict-a.js
@@ -4,91 +4,91 @@
 		"word":"A surge of anger",
 		"type": "[Noun + Noun]",
 		"meaning": "a powerful rush of an emotion or feeling of anger.",
-		"example": "Susan felt a surge of anger.<br> Lucy felt a surge of anger as she watched her ex-husband enter the room with his new girlfriend."
+		"example": "<p>Susan felt a surge of anger.</p><p> Lucy felt a surge of anger as she watched her ex-husband enter the room with his new girlfriend.</p>"
 	},
 	{
 		"id": 1,
 		"word":"A sense of pride",
 		"type": "[Noun + Noun]",
 		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community.<br> When people trust you, you will naturally feel a sense of confidence that will spur your sense of pride at work."
+		"example": "<p>She felt a sense of pride after she had finished the assignment.</p><p> Doing well in school gives students a sense of pride.</p><p> We have a sense of pride in our community.</p><p>When people trust you, you will naturally feel a sense of confidence that will spur your sense of pride at work.</p>"
 	},
 	{
 		"id": 2,
 		"word":"A streaming cold",
 		"type": "[Adjective + Noun]",
 		"meaning": "a heavy cold.",
-		"example": "He’s got a streaming cold.<br> She has caught a streaming cold.<br> She has been in bed with a streaming cold."
+		"example": "<p>He’s got a streaming cold.</p><p>She has caught a streaming cold.</p><p>She has been in bed with a streaming cold.</p>"
 	},
 	{
 		"id": 3,
 		"word":"A massive heart attack",
 		"type": "[Adjective + Noun]",
 		"meaning": "A very serious heart attack.",
-		"example": "She died of a massive heart attack last year.<br> No one would like to suffer from a massive heart attack."
+		"example": "<p>She died of a massive heart attack last year.</p><p>No one would like to suffer from a massive heart attack.</p>"
 	},
 	{
 		"id": 4,
 		"word":"Absolutely/utterly ridiculous",
 		"type": "[Adverb + Adjective]",
 		"meaning": "Very silly or completely unreasonable.",
-		"example": "He looked utterly ridiculous when he dressed like a teenager. <br> She looks absolutely ridiculous in that yellow hat."
+		"example": "<p>He looked utterly ridiculous when he dressed like a teenager. </p><p>She looks absolutely ridiculous in that yellow hat.</p>"
 	},
 	{
 		"id": 5,
 		"word":"A decline in demand",
 		"type": "[Noun + Noun]",
 		"meaning": "A fall in demand.",
-		"example": "There was a decline in demand for female services in some occupations. <br> An increase in automobile prices may lead to a decline in demand for gas."
+		"example": "<p>There was a decline in demand for female services in some occupations. </p><p>An increase in automobile prices may lead to a decline in demand for gas.</p>"
 	},
 	{
 		"id": 6,
 		"word":"Absolutely/utterly stupid",
 		"type": "[Adverb + Adjective]",
 		"meaning": "Very silly.",
-		"example": "Jack was utterly stupid to quit his job like that. <br> That guys behavior was absolutely stupid."
+		"example": "<p>Jack was utterly stupid to quit his job like that. </p><p>That guys behavior was absolutely stupid.</p>"
 	},
 	{
 		"id": 7,
 		"word":"Absolutely/utterly impossible",
 		"type": "[Adverb + Adjective]",
 		"meaning": "Very badly.",
-		"example": "Jack was utterly stupid to quit his job like that. <br> That guy's behavior was absolutely stupid."
+		"example": "<p>Jack was utterly stupid to quit his job like that. </p><p>That guy's behavior was absolutely stupid.</p>"
 	},
 	{
 		"id": 8,
 		"word":"Absolutely necessary",
 		"type": "[Adverb + Adjective]",
 		"meaning": "Totally or completely essential or needed to do something, provide something, or make something happen.",
-		"example": "Are you sure the operation is absolutely necessary?<br>The operation is absolutely necessary for him at the moment.<br>If it wasn't absolutely necessary, we wouldn't be doing it.<br> He doesn't want to be disturbed unless something is absolutely necessary."
+		"example": "<p>Are you sure the operation is absolutely necessary?</p><p>The operation is absolutely necessary for him at the moment.</p><p>If it wasn't absolutely necessary, we wouldn't be doing it.</p><p> He doesn't want to be disturbed unless something is absolutely necessary.</p>"
 	},
 	{
 		"id": 9,
 		"word":"Abuse drugs",
 		"type": "[Verb + Noun]",
 		"meaning": "to use drugs in a way that is harmful to yourself or others.",
-		"example": "Mario abused drugs for many years, but luckily he got some help and stopped.<br>Prescription drugs that relieve pain are being abused a lot these days.<br>He died early due to abusing drugs for many years. <br> Bill has abused drugs for over 5 years."
+		"example": "<p>Mario abused drugs for many years, but luckily he got some help and stopped.</p><p>Prescription drugs that relieve pain are being abused a lot these days.</p><p>He died early due to abusing drugs for many years. </p><p>Bill has abused drugs for over 5 years.</p>"
 	},
 	{
 		"id": 10,
 		"word":"Abuse of power",
 		"type": "[Noun + Noun]",
 		"meaning": "harmful, unethical or improper use of power by someone who has that authority",
-		"example": "The president was accused of abusing his power after ordering soldiers to shoot at protesters. <br> He was charged with abuse of power to keep prices artificially high. <br>Police directives about what can and can't be photographed are an abuse of power and should be ignored, Liberty Victoria says.<br> It is dangerous for citizens to complain about corruption or abuse of power by public officials. <br>Toxic leadership is all about the abuse of power and its destructiveness.<br>The president was charged with abuse of power after ordering soldiers to shoot at protesters."
+		"example": "<p>The president was accused of abusing his power after ordering soldiers to shoot at protesters. </p><p> He was charged with abuse of power to keep prices artificially high. </p><p>Police directives about what can and can't be photographed are an abuse of power and should be ignored, Liberty Victoria says.</p><p> It is dangerous for citizens to complain about corruption or abuse of power by public officials. </p><p>Toxic leadership is all about the abuse of power and its destructiveness.</p><p>The president was charged with abuse of power after ordering soldiers to shoot at protesters.</p>"
 	},
 	{
 		"id": 11,
 		"word":"Accept (a) defeat",
 		"type": "[Verb + Noun]",
 		"meaning": "to accept the fact that you cannot succeed in doing something and stop trying to do it.",
-		"example": "Talent and refusal to accept defeat was written on your face and your body in every game you played.<br>There are times when you have to accept defeat because of the resilience your enemy has shown even against best of your damage spells.<br>He finally had to accept a defeat<br>Tom thought he couldn't win the game, so he had to accept a defeat."
+		"example": "<p>Talent and refusal to accept defeat was written on your face and your body in every game you played.</p><p>There are times when you have to accept defeat because of the resilience your enemy has shown even against best of your damage spells.</p><p>He finally had to accept a defeat</p><p>Tom thought he couldn't win the game, so he had to accept a defeat.</p>"
 	},
 	{
 		"id": 12,
 		"word":"Accept (a) challenge",
 		"type": "[Verb + Noun]",
 		"meaning": "to agree to do something difficult or dangerous.",
-		"example": "Being captain of the team won't be easy, but I'm sure David will accept the challenge.<br>This really big guy challenged me to a fight, but instead accepting I ran away as fast as I could."
+		"example": "<p>Being captain of the team won't be easy, but I'm sure David will accept the challenge.</p><p>This really big guy challenged me to a fight, but instead accepting I ran away as fast as I could.</p>"
 	},
 	{
 		"id": 13,
