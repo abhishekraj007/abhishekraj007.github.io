b653a3d39c548bf7ceb478f1600b8efafc1a43c0
Abhishek Raj <abhishek@lollypop.biz>
2017-09-29 23:54:25 +0530

    update

--- apps/engmania/dict-a.js
+++ apps/engmania/dict-a.js
@@ -11,7 +11,7 @@
 		"word":"A sense of pride",
 		"type": "[Noun + Noun]",
 		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
+		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community.<br> When people trust you, you will naturally feel a sense of confidence that will spur your sense of pride at work."
 	},
 	{
 		"id": 2,
@@ -25,133 +25,84 @@
 		"word":"A massive heart attack",
 		"type": "[Adjective + Noun]",
 		"meaning": "A very serious heart attack.",
-		"example": "She died of a massive heart attack last year.</br> No one would like to suffer from a massive heart attack."
+		"example": "She died of a massive heart attack last year.<br> No one would like to suffer from a massive heart attack."
 	},
 	{
 		"id": 4,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 5,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 6,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 7,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 8,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 9,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 10,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 11,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 12,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 13,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 14,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 15,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 16,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 17,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 18,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 19,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 20,
-		"word":"A sense of pride",
-		"type": "[Noun + Noun]",
-		"meaning": "a feeling of pleasure and satisfaction that you get when you, or someone connected with you, have done or achieved something good.",
-		"example": "She felt a sense of pride after she had finished the assignment.<br> Doing well in school gives students a sense of pride.<br> We have a sense of pride in our community."
-	},
-	{
-		"id": 21,
 		"word":"Absolutely/utterly ridiculous",
 		"type": "[Adverb + Adjective]",
 		"meaning": "Very silly or completely unreasonable.",
-		"example": "He looked utterly ridiculous when he dressed like a teenager. </br> She looks absolutely ridiculous in that yellow hat."
-	}
+		"example": "He looked utterly ridiculous when he dressed like a teenager. <br> She looks absolutely ridiculous in that yellow hat."
+	},
+	{
+		"id": 5,
+		"word":"A decline in demand",
+		"type": "[Noun + Noun]",
+		"meaning": "A fall in demand.",
+		"example": "There was a decline in demand for female services in some occupations. <br> An increase in automobile prices may lead to a decline in demand for gas."
+	},
+	{
+		"id": 6,
+		"word":"Absolutely/utterly stupid",
+		"type": "[Adverb + Adjective]",
+		"meaning": "Very silly.",
+		"example": "Jack was utterly stupid to quit his job like that. <br> That guys behavior was absolutely stupid."
+	},
+	{
+		"id": 7,
+		"word":"Absolutely/utterly impossible",
+		"type": "[Adverb + Adjective]",
+		"meaning": "Very badly.",
+		"example": "Jack was utterly stupid to quit his job like that. <br> That guy's behavior was absolutely stupid."
+	},
+	{
+		"id": 8,
+		"word":"Absolutely necessary",
+		"type": "[Adverb + Adjective]",
+		"meaning": "Totally or completely essential or needed to do something, provide something, or make something happen.",
+		"example": "Are you sure the operation is absolutely necessary?<br>The operation is absolutely necessary for him at the moment.<br>If it wasn't absolutely necessary, we wouldn't be doing it.<br> He doesn't want to be disturbed unless something is absolutely necessary."
+	},
+	{
+		"id": 9,
+		"word":"Abuse drugs",
+		"type": "[Verb + Noun]",
+		"meaning": "to use drugs in a way that is harmful to yourself or others.",
+		"example": "Mario abused drugs for many years, but luckily he got some help and stopped.<br>Prescription drugs that relieve pain are being abused a lot these days.<br>He died early due to abusing drugs for many years. <br> Bill has abused drugs for over 5 years."
+	},
+	{
+		"id": 10,
+		"word":"Abuse of power",
+		"type": "[Noun + Noun]",
+		"meaning": "harmful, unethical or improper use of power by someone who has that authority",
+		"example": "The president was accused of abusing his power after ordering soldiers to shoot at protesters. <br> He was charged with abuse of power to keep prices artificially high. <br>Police directives about what can and can't be photographed are an abuse of power and should be ignored, Liberty Victoria says.<br> It is dangerous for citizens to complain about corruption or abuse of power by public officials. <br>Toxic leadership is all about the abuse of power and its destructiveness.<br>The president was charged with abuse of power after ordering soldiers to shoot at protesters."
+	},
+	{
+		"id": 11,
+		"word":"Accept (a) defeat",
+		"type": "[Verb + Noun]",
+		"meaning": "to accept the fact that you cannot succeed in doing something and stop trying to do it.",
+		"example": "Talent and refusal to accept defeat was written on your face and your body in every game you played.<br>There are times when you have to accept defeat because of the resilience your enemy has shown even against best of your damage spells.<br>He finally had to accept a defeat<br>Tom thought he couldn't win the game, so he had to accept a defeat."
+	},
+	{
+		"id": 12,
+		"word":"Accept (a) challenge",
+		"type": "[Verb + Noun]",
+		"meaning": "to agree to do something difficult or dangerous.",
+		"example": "Being captain of the team won't be easy, but I'm sure David will accept the challenge.<br>This really big guy challenged me to a fight, but instead accepting I ran away as fast as I could."
+	},
+	{
+		"id": 13,
+		"word":"Accept (an) apology",
+		"type": "[Verb + Noun]",
+		"meaning": "to forgive someone who says they're sorry for doing something wrong or for causing a problem.",
+		"example": "My mistake was serious, but luckily the boss accepted my apology.<br>She refused to accept her husband's apology and said she wanted a divorce.<br>Paul refused to accept his wife's apology and said he wanted a divorce."
+	},
+	{
+		"id": 14,
+		"word":"Accept an invitation",
+		"type": "[Verb + Noun]",
+		"meaning": "to say 'yes' after getting an invitation/to agree to spend time with someone socially or to come to a social event after getting an invitation.",
+		"example": "Most of the people we invited to the party have accepted their invitation already.<br>Tom accepted an invitation to the party."
+	},
 
 ]
