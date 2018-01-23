2b6334aa9fadd7b4676b006179d276deff51723a
Abhishek Raj <abhishek@lollypop.biz>
2018-01-23 21:18:56 +0530

    added latest code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,22 @@
-$(document).ready(function(){
- 
-    AOS.init();
-    
-    // On load add plus btn
-    var $showBtn = $(".show-project");
-    var $hideBtn = $(".hide-project");
-    var $projectHidden = $('.project-hidden');
-    
-    $projectHidden.hide();
-    
-    // var $plusBtn = "<i class='fa fa-plus-circle'></i>";
-    // var $crossBtn = "<i class='fa fa-times-circle'></i>";
-    // $showBtn.append($plusBtn);
-    // $hideBtn.append($crossBtn);
-    
-   $showBtn.on('click', function(){
-       $projectHidden.fadeIn().delay(500).show();
-       $showBtn.hide();
-       
-   });
-   
-   $hideBtn.on('click', function(){
-       $projectHidden.fadeOut(1000).delay(500).hide();
-       $showBtn.show();
-   })
-   
-   
-    
-    
-    
-    
-     
-     
-     
-    
-});
+
+{
+	setTimeout(() => document.body.classList.add('render'), 60);
+
+
+	function toggleChat() {
+		var chatButton = document.querySelector('.chat-container');
+		chatButton.classList.toggle('active');
+	}
+
+	function toggleLatestWork() {
+		var latestProjectCard = document.querySelector('.latest-project');
+
+		latestProjectCard.classList.toggle('hide');
+	}
+
+	// chatButton.addEventListner('click', () => {
+	// })
+
+
+}
+
